a96adf48edb8a54eb0c7d2b7c2922036b042846e
TheSoulrester <alex.gropp@icloud.com>
2025-06-27 08:47:39 +0200

    Developing a basic component
    Entwicklung einer Basis MVC Komponente

---
+++ admin/src/Controller/DisplayController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AlexanderGropp\Component\BlaulichtMonitor\Administrator\Controller;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\BaseController;
+
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_blaulichtmonitor
+ *
+ * @copyright   Copyright (C) 2025 Alexander Gropp. All rights reserved.
+ * @license     GNU General Public License version 3; see LICENSE
+ */
+
+/**
+ * Default Controller of BlaulichtMonitor component
+ *
+ * @package     Joomla.Administrator
+ * @subpackage  com_blaulichtmonitor
+ */
+class DisplayController extends BaseController
+{
+    /**
+     * The default view for the display method.
+     *
+     * @var string
+     */
+    protected $default_view = 'Einsatzberichte';
+
+    public function display($cachable = false, $urlparams = array())
+    {
+        return parent::display($cachable, $urlparams);
+    }
+}
