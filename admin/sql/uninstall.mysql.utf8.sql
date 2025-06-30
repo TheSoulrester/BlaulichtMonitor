e587de888d3d8fe9617db56c148b8ebbc6f7896b
TheSoulrester <alex.gropp@icloud.com>
2025-06-30 22:27:15 +0200

    minor changes at namespaces

--- admin/sql/uninstall.mysql.utf8.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE IF EXISTS `#__blaulichtmonitor_greetings`;
--- admin/src/Extension/BlaulichtMonitor.php
+++ admin/src/Extension/BlaulichtMonitor.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Piedpiper\Component\Spm\Administrator\Extension;
+namespace AlexanderGropp\Component\BlaulichtMonitor\Administrator\Extension;
 
 use Joomla\CMS\Extension\BootableExtensionInterface;
 use Joomla\CMS\Extension\MVCComponent;
 use Psr\Container\ContainerInterface;
 
-class SpmComponent extends MVCComponent implements BootableExtensionInterface
+class BlaulichtMonitorComponent extends MVCComponent implements BootableExtensionInterface
 {
     public function boot(ContainerInterface $container) {}
 }
