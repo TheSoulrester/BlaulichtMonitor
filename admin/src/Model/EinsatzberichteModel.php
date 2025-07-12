0c664ccfcd3569e12203265e76b6f6037e4a8ba2
TheSoulrester <alex.gropp@icloud.com>
2025-07-12 23:41:50 +0200

    Some code improvements for einsatzberichte

--- admin/src/Model/EinsatzberichteModel.php
+++ admin/src/Model/EinsatzberichteModel.php
@@ -22,8 +22,25 @@ class EinsatzberichteModel extends ListModel
 				'a.id',
 				'alarmierungszeit',
 				'a.alarmierungszeit',
+				'einsatzart_id',
+				'a.einsatzart_id',
+				'einsatzart_title',
+				'b.title',
+				'einsatzort_strasse',
+				'a.einsatzort_strasse',
+				'einsatzkurzbericht',
+				'a.einsatzkurzbericht',
 				'counter_clicks',
 				'a.counter_clicks',
+				'einheiten_liste',
+				'created_by',
+				'a.created_by',
+				'created_by_name',
+				'uc.name',
+				'modified_by',
+				'a.modified_by',
+				'modified_by_name',
+				'um.name',
 			];
 		}
 		parent::__construct($config);
@@ -69,6 +86,8 @@ class EinsatzberichteModel extends ListModel
 					$db->quoteName('a.counter_clicks'),
 					$db->quoteName('a.created'),
 					$db->quoteName('a.modified'),
+					$db->quoteName('a.created_by'),
+					$db->quoteName('a.modified_by'),
 					// Titel der Einsatzart
 					$db->quoteName('b.title', 'einsatzart_title'),
 					// Einheiten als kommaseparierte Liste
@@ -77,10 +96,17 @@ class EinsatzberichteModel extends ListModel
                         INNER JOIN #__blaulichtmonitor_einheiten e ON ee.einheit_id = e.id
                         WHERE ee.einsatzbericht_id = a.id
                     ) AS einheiten_liste',
+					// Usernamen für created_by und modified_by
+					$db->quoteName('uc.name', 'created_by_name'),
+					$db->quoteName('um.name', 'modified_by_name'),
 				]
 			)
 		)->from($db->quoteName('#__blaulichtmonitor_einsatzberichte', 'a'))
-			->join('LEFT', $db->quoteName('#__blaulichtmonitor_einsatzarten', 'b') . ' ON ' . $db->quoteName('a.einsatzart_id') . ' = ' . $db->quoteName('b.id'));
+			->join('LEFT', $db->quoteName('#__blaulichtmonitor_einsatzarten', 'b') . ' ON ' . $db->quoteName('a.einsatzart_id') . ' = ' . $db->quoteName('b.id'))
+			// Join für created_by
+			->join('LEFT', $db->quoteName('#__users', 'uc') . ' ON ' . $db->quoteName('uc.id') . ' = ' . $db->quoteName('a.created_by'))
+			// Join für modified_by
+			->join('LEFT', $db->quoteName('#__users', 'um') . ' ON ' . $db->quoteName('um.id') . ' = ' . $db->quoteName('a.modified_by'));
 
 		// Filter: Suchbegriff
 		$search = $this->getState('filter.search');
@@ -90,12 +116,22 @@ class EinsatzberichteModel extends ListModel
 				'%',
 				$db->escape(trim($search), true) . '%'
 			));
-			$query->where('(a.alarmierungszeit LIKE ' . $search .
-				' OR a.einsatzort_strasse LIKE ' . $search .
-				' OR a.einsatzkurzbericht LIKE ' . $search . ')');
+
+			$where = [
+				'a.id LIKE ' . $search,
+				'a.alarmierungszeit LIKE ' . $search,
+				'b.title LIKE ' . $search, // Einsatzart
+				'a.einsatzort_strasse LIKE ' . $search,
+				'a.einsatzkurzbericht LIKE ' . $search,
+				'(SELECT GROUP_CONCAT(e.title SEPARATOR ", ") FROM #__blaulichtmonitor_einsatzberichte_einheiten ee INNER JOIN #__blaulichtmonitor_einheiten e ON ee.einheit_id = e.id WHERE ee.einsatzbericht_id = a.id) LIKE ' . $search,
+				'uc.name LIKE ' . $search, // created_by_name
+				'um.name LIKE ' . $search, // modified_by_name
+			];
+
+			$query->where('(' . implode(' OR ', $where) . ')');
 		}
 
-		// Sortierung
+		// Default-Sortierung nach Alarmierungszeit
 		$orderCol  = $this->state->get('list.ordering', 'a.alarmierungszeit');
 		$orderDirn = $this->state->get('list.direction', 'DESC');
 		$query->order($db->escape($orderCol) . ' ' . $db->escape($orderDirn));
--- admin/tmpl/einsatzberichte/default.php
+++ admin/tmpl/einsatzberichte/default.php
@@ -11,6 +11,10 @@ use Joomla\CMS\Router\Route;
  * Für weitere Views kann dieses Template kopiert und angepasst werden.
  */
 
+/** @var \Joomla\CMS\WebAsset\WebAssetManager $wa */
+$wa = $this->getDocument()->getWebAssetManager();
+$wa->useScript('table.columns'); // Implementierung eines Buttons zum Anzeigen/Ausblenden von Spalten
+
 $listOrder = $this->escape($this->state->get('list.ordering'));
 $listDirn  = $this->escape($this->state->get('list.direction'));
 ?>
@@ -21,77 +25,110 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 			<div id="j-main-container" class="j-main-container">
 				<!-- Filter- und Suchformular -->
 				<?php echo LayoutHelper::render('joomla.searchtools.default', ['view' => $this]); ?>
+				<h1 hidden class="page-title">Einsatzberichte</h1>
+				<div class="table-responsive">
+					<table class="table table-striped itemList" id="articleList">
+						<caption>BlaulichtMonitor Einsatzberichte</caption>
+						<caption class="visually-hidden">BlaulichtMonitor Einsatzberichte
+							<span id="orderedBy">Sortiert nach </span>
+							<span id="filteredBy">Gefiltert nach </span>
+						</caption>
+						<thead>
+							<tr>
+								<th scope="col" class="text-center">
+									<?php echo HTMLHelper::_('searchtools.sort', 'ID', 'a.id', $listDirn, $listOrder); ?>
+								</th>
+								<th scope="col" class="text-center">Status</th>
+								<th scope="col" class="">
+									<?php echo HTMLHelper::_('searchtools.sort', 'Alarmierungszeit', 'a.alarmierungszeit', $listDirn, $listOrder); ?>
+								</th>
+								<th scope="col" class="">Einsatzart</th>
+								<th scope="col" class="">Einsatzort</th>
+								<th scope="col" class="">Kurzbericht</th>
+								<th scope="col" class="text-center">Einheiten</th>
+								<th scope="col" class="text-center">
+									<?php echo HTMLHelper::_('searchtools.sort', 'Zugriffe', 'a.counter_clicks', $listDirn, $listOrder); ?>
+								</th>
+								<th scope="col" class="d-none">Erstellt</th>
+								<th scope="col" class="">Bearbeitet</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php foreach ($this->items as $item) : ?>
+								<tr>
+									<td class="text-center"><?php echo '<span class="badge bg-primary border">#' . $item->id . '</span>'; ?></td>
+									<td class="text-center">
+										<?php echo HTMLHelper::_('jgrid.published', $item->veroeffentlicht, $i, 'einsatzberichte.', $canChange, 'cb', $item->publish_up, $item->publish_down); ?>
+									</td>
+									<td>
+										<?php
+										$dt_alarmierungszeit = \DateTime::createFromFormat('Y-m-d H:i:s', $item->alarmierungszeit);
+										if ($dt_alarmierungszeit) {
+											echo $dt_alarmierungszeit->format('d.m.Y') . '<br>';
+											echo $dt_alarmierungszeit->format('H:i') . ' Uhr';
+										} else {
+											echo htmlspecialchars($item->alarmierungszeit);
+										}
+										?>
+									</td>
+									<td><a href="<?php echo Route::_('/administrator/index.php?option=com_blaulichtmonitor&task=einsatzbericht.edit&id=' . $item->id); ?>"><?php echo $item->einsatzart_title; ?></a></td>
+									<td><?php echo $item->einsatzort_strasse; ?></td>
+									<td><?php echo $item->einsatzkurzbericht; ?></td>
+									<td class="text-center">
+										<div class="d-flex flex-wrap justify-content-between gap-1">
+											<?php $einheiten = explode(',', $item->einheiten_liste);
+											foreach ($einheiten as $einheit) {
+												$einheit = trim($einheit);
+												if ($einheit) {
+													echo '<span class="flex-fill badge bg-primary border">' . htmlspecialchars($einheit) . '</span>';
+												}
+											} ?>
+										</div>
+									</td>
+									<td class="text-center"><span class="badge bg-success fs-5"><?php echo $item->counter_clicks; ?></span></td>
+									<td>
+										<div class="d-flex flex-column">
+											<?php
+											$dt_created = \DateTime::createFromFormat('Y-m-d H:i:s', $item->created);
+											if ($dt_created) {
+												echo '<span>' . $dt_created->format('d.m.Y') . '</span>';
+												echo '<span>' . $dt_created->format('H:i') . ' Uhr</span>';
+											} else {
+												echo '<span>' . htmlspecialchars($item->created) . '</span>';
+											}
+											?>
+											<?php if (!empty($item->created_by_name)) : ?>
+												<small class="text-muted text-truncate" style="max-width: 120px;">
+													<?php echo htmlspecialchars($item->created_by_name); ?>
+												</small>
+											<?php endif; ?>
+										</div>
+									</td>
+									<td>
+										<div class="d-flex flex-column">
+											<?php
+											$dt_modified = \DateTime::createFromFormat('Y-m-d H:i:s', $item->modified);
+											if ($dt_modified) {
+												echo '<span>' . $dt_modified->format('d.m.Y') . '</span>';
+												echo '<span>' . $dt_modified->format('H:i') . ' Uhr</span>';
+											} else {
+												echo '<span>' . htmlspecialchars($item->modified) . '</span>';
+											}
+											?>
+											<?php if (!empty($item->modified_by_name)) : ?>
+												<small class="text-muted text-truncate" style="max-width: 120px;">
+													<?php echo htmlspecialchars($item->modified_by_name); ?>
+												</small>
+											<?php endif; ?>
+										</div>
+									</td>
+								</tr>
+							<?php endforeach; ?>
+						</tbody>
+					</table>
+				</div>
 			</div>
 		</div>
-	</div>
-	<div class="table-responsive">
-		<!-- Tabelle mit allen Einsatzberichten -->
-		<table class="table table-striped">
-			<caption>BlaulichtMonitor Einsatzberichte</caption>
-			<thead>
-				<tr>
-					<th>
-						<!-- Sortierbarer Spaltenkopf: ID -->
-						<?php echo HTMLHelper::_('searchtools.sort', 'ID', 'a.id', $listDirn, $listOrder); ?>
-					</th>
-					<th>Status</th>
-					<th>
-						<!-- Sortierbarer Spaltenkopf: Alarmierungszeit -->
-						<?php echo HTMLHelper::_('searchtools.sort', 'Alarmierungszeit', 'a.alarmierungszeit', $listDirn, $listOrder); ?>
-					</th>
-					<th>Einsatzart</th>
-					<th>Einsatzort</th>
-					<th>Kurzbericht</th>
-					<th>Einheiten</th>
-					<th>
-						<!-- Sortierbarer Spaltenkopf: Zugriffe -->
-						<?php echo HTMLHelper::_('searchtools.sort', 'Zugriffe', 'a.counter_clicks', $listDirn, $listOrder); ?>
-					</th>
-					<th>Erstellt am</th>
-					<th>Bearbeitet am</th>
-				</tr>
-			</thead>
-			<tbody>
-				<!-- Zeilen für jeden Einsatzbericht -->
-				<?php foreach ($this->items as $item) : ?>
-					<tr>
-						<td><?php echo $item->id; ?></td>
-						<td>
-							<!-- Status (veröffentlicht/nicht veröffentlicht) -->
-							<?php echo HTMLHelper::_('jgrid.published', $item->veroeffentlicht, $i, 'einsatzberichte.', $canChange, 'cb', $item->publish_up, $item->publish_down); ?>
-						</td>
-						<td>
-							<?php $dt_alarmierungszeit = \DateTime::createFromFormat('Y-m-d H:i:s', $item->alarmierungszeit);
-							echo $dt_alarmierungszeit ? $dt_alarmierungszeit->format('d.m.Y H:i:s') : htmlspecialchars($item->alarmierungszeit); ?>
-						</td>
-						<td><a href="<?php echo Route::_('/administrator/index.php?option=com_blaulichtmonitor&task=einsatzbericht.edit&id=' . $item->id); ?>"><?php echo $item->einsatzart_title; ?></a></td>
-						<td><?php echo $item->einsatzort_strasse; ?></td>
-						<td><?php echo $item->einsatzkurzbericht; ?></td>
-						<td>
-							<div class="d-flex flex-wrap justify-content-between gap-1">
-								<!-- Einheiten als Badges -->
-								<?php $einheiten = explode(',', $item->einheiten_liste);
-								foreach ($einheiten as $einheit) {
-									$einheit = trim($einheit);
-									if ($einheit) {
-										echo '<span class="flex-fill badge bg-primary border">' . htmlspecialchars($einheit) . '</span>';
-									}
-								} ?>
-							</div>
-						</td>
-						<td><span class="badge bg-success fs-5"><?php echo $item->counter_clicks; ?></span></td>
-						<td>
-							<?php $dt_created = \DateTime::createFromFormat('Y-m-d H:i:s', $item->created);
-							echo $dt_created ? $dt_created->format('d.m.Y H:i:s') : htmlspecialchars($item->created); ?>
-						</td>
-						<td>
-							<?php $dt_modified = \DateTime::createFromFormat('Y-m-d H:i:s', $item->modified);
-							echo $dt_modified ? $dt_modified->format('d.m.Y H:i:s') : htmlspecialchars($item->modified); ?>
-						</td>
-					</tr>
-				<?php endforeach; ?>
-			</tbody>
-		</table>
 	</div>
 	<!-- Pagination -->
 	<?php echo $this->pagination->getListFooter(); ?>
