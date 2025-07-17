fda52a03dddfb0efddb229068df16dcf9f3464a6
TheSoulrester <alex.gropp@icloud.com>
2025-07-17 13:09:40 +0200

    some edits on einsatzbericht forms

--- admin/src/Table/EinsatzberichtTable.php
+++ admin/src/Table/EinsatzberichtTable.php
@@ -7,8 +7,31 @@ use Joomla\Database\DatabaseDriver;
 
 class EinsatzberichtTable extends Table
 {
-    public function __construct(DatabaseDriver $db)
-    {
-        parent::__construct('#__blaulichtmonitor_einsatzberichte', 'id', $db);
-    }
+	public function __construct(DatabaseDriver $db)
+	{
+		parent::__construct('#__blaulichtmonitor_einsatzberichte', 'id', $db);
+	}
+
+	public function bind($array, $ignore = '')
+	{
+		// Leere Datumsfelder auf null setzen
+		foreach (['alarmierungszeit', 'ausrueckzeit', 'einsatzende'] as $field) {
+			if (isset($array[$field]) && $array[$field] === '') {
+				$array[$field] = null;
+			}
+		}
+		// Leere Zahlenfelder auf null setzen
+		foreach (['people_count'] as $field) {
+			if (isset($array[$field]) && $array[$field] === '') {
+				$array[$field] = null;
+			}
+		}
+		// Leere Priorität auf null setzen
+		foreach (['prioritaet'] as $field) {
+			if (isset($array[$field]) && $array[$field] === '') {
+				$array[$field] = null;
+			}
+		}
+		return parent::bind($array, $ignore);
+	}
 }
--- admin/tmpl/einsatzbericht/edit.php
+++ admin/tmpl/einsatzbericht/edit.php
@@ -7,20 +7,20 @@ HTMLHelper::_('behavior.formvalidator');
 HTMLHelper::_('behavior.keepalive');
 ?>
 <form action="<?php echo Route::_('index.php?option=com_blaulichtmonitor&view=einsatzbericht&layout=edit&id=' . (int) $this->item->id); ?>" method="post" name="adminForm" id="project-form" class="form-validate">
-    <div>
-        <div class="row">
-            <div class="col-md-9">
-                <div class="row">
-                    <div class="col-md-6">
-                        <?php
-                        foreach ($this->form->getFieldset() as $field) : ?>
-                            <?php echo $field->renderField(); ?>
-                        <?php endforeach; ?>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <input type="hidden" name="task" value="">
-    <?php echo HTMLHelper::_('form.token'); ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-9">
+				<div class="row">
+					<div class="col-md-9">
+						<?php
+						foreach ($this->form->getFieldset() as $field) : ?>
+							<?php echo $field->renderField(); ?>
+						<?php endforeach; ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<input type="hidden" name="task" value="">
+	<?php echo HTMLHelper::_('form.token'); ?>
 </form>
